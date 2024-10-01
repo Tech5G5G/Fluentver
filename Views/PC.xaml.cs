9b4c84446b33a1b8d3195d64adf108d27d0969f3
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-09-30 17:37:47 -0700

    Event handlers for changing window height

--- Views/PC.xaml.cs
+++ Views/PC.xaml.cs
@@ -70,6 +70,29 @@ namespace Fluentver.Views
             };
             timer.Start();
         }
+
+        private void PCInfo_WindowHeight_Increase(Expander sender, ExpanderExpandingEventArgs args)
+        {
+            MainWindow mw = (MainWindow)((App)(Application.Current)).m_window;
+            if (pcInfo.XamlRoot is not null)
+            {
+                SizeInt32 newSize = new SizeInt32();
+                newSize.Width = mw.AppWindow.Size.Width;
+                newSize.Height = mw.AppWindow.Size.Height + (int)(90 * pcInfo.XamlRoot.RasterizationScale);
+                mw.AppWindow.Resize(newSize);
+            }
+        }
+
+        private void PCInfo_WindowHeight_Decrease(Expander sender, ExpanderCollapsedEventArgs args)
+        {
+            MainWindow mw = (MainWindow)((App)(Application.Current)).m_window;
+            if (pcInfo.XamlRoot is not null)
+            {
+                SizeInt32 newSize = new SizeInt32();
+                newSize.Width = mw.AppWindow.Size.Width;
+                newSize.Height = mw.AppWindow.Size.Height - (int)(90 * pcInfo.XamlRoot.RasterizationScale);
+                mw.AppWindow.Resize(newSize);
+            }
         }
     }
 }
