50c63919758519bd3fb508b8d3b7a048165d6044
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-10-04 12:57:59 -0700

    Added UIElement UserEntry

--- App.xaml.cs
+++ App.xaml.cs
@@ -33,6 +33,38 @@ using Windows.UI.ViewManagement;
 
 namespace Fluentver
 {
+    public sealed class UserEntry : Control
+    {
+        public UserEntry()
+        {
+            this.DefaultStyleKey = typeof(UserEntry);
+        }
+
+        public ImageSource ProfilePicture
+        {
+            get { return (ImageSource)GetValue(ProfilePictureProperty); }
+            set { SetValue(ProfilePictureProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfilePictureProperty = DependencyProperty.Register("ProfilePicture", typeof(ImageSource), typeof(UserEntry), new PropertyMetadata(null));
+
+        public string DisplayName
+        {
+            get { return (string)GetValue(DisplayNameProperty); }
+            set { SetValue(DisplayNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisplayNameProperty = DependencyProperty.Register("DisplayName", typeof(string), typeof(UserEntry), new PropertyMetadata(string.Empty));
+
+        public string AccountName
+        {
+            get { return (string)GetValue(AccountNameProperty); }
+            set { SetValue(AccountNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty AccountNameProperty = DependencyProperty.Register("AccountName", typeof(string), typeof(UserEntry), new PropertyMetadata(string.Empty));
+    }
+
     public sealed class GlyphButton : Control
     {
         public GlyphButton()
