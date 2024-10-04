b3aeab8b2f8f81d781b8681537a7ad42bb092e44
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-10-04 12:57:29 -0700

    Added usage

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
 using System;
