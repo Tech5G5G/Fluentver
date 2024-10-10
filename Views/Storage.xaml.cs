2866beeb78f9ba4faca42343c356ecb29a84002a
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2024-10-10 16:49:48 -0700

    Added usage

--- Views/Storage.xaml.cs
+++ Views/Storage.xaml.cs
@@ -14,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System.Diagnostics;
 using Microsoft.UI;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
