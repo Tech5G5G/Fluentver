e6e32eb747247536980f9c004b6a0934c2a6b64b
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-23 16:20:53 -0700

    Add InfoPage base class

---
+++ Fluentver/Controls/InfoPage.cs
@@ -0,0 +1,18 @@
+﻿namespace Fluentver.Controls;
+
+public partial class InfoPage : Page
+{
+    public IList<GlyphButton> ToolbarButtons { get; } = [];
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+
+        if (e.Parameter is MainWindow mw)
+        {
+            mw.ToolbarButtons.Clear();
+            foreach (var button in ToolbarButtons)
+                mw.ToolbarButtons.Add(button);
+        }
+    }
+}
