e23d4653efb8ce9955af2578c287d8dd1e7188bc
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-23 16:21:16 -0700

    Add ControlExtensions

---
+++ Fluentver/Extensions/ControlExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Fluentver.Extensions;
+
+public static class ControlExtensions
+{
+    public static int GetSelectedIndex(this SelectorBar bar) => bar.Items.IndexOf(bar.SelectedItem);
+
+    public static void SetSelectedIndex(this SelectorBar bar, int index)
+    {
+        if (index >= 0 && index < bar.Items.Count)
+            bar.SelectedItem = bar.Items[index];
+        else
+            bar.SelectedItem = null;
+    }
+
+    public static GlyphButton AddClick(this GlyphButton button, RoutedEventHandler handler)
+    {
+        button.Click += handler;
+        return button;
+    }
+}
