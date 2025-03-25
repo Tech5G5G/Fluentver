8fd25fc4f9cabd531f2017541b7a37d13fbfafc4
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-24 18:46:56 -0700

    Add SettingsPage basic layout

---
+++ Fluentver/Settings/SettingsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Fluentver.Settings
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
