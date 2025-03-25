4e396022745db001fccfe27f70b2d842ea934534
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-24 18:46:42 -0700

    Temp code to switch between main content and settings

--- Fluentver/MainWindow.xaml.cs
+++ Fluentver/MainWindow.xaml.cs
@@ -66,5 +66,54 @@ namespace Fluentver
             new SlideNavigationTransitionInfo { Effect = previousIndex - currentIndex > 0 ? SlideNavigationTransitionEffect.FromLeft : SlideNavigationTransitionEffect.FromRight });
             previousIndex = currentIndex;
         }
+
+        //TODO: Fix this code
+        StackPanel mainContent;
+        StackPanel settingsPage;
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainContent is null && settingsPage is null) //Create settings page and set content to it
+            {
+                mainContent = Content as StackPanel;
+                mainContent.Children.Remove(titleBarGrid);
+
+                Content = settingsPage = new()
+                {
+                    Children =
+                    {
+                        titleBarGrid,
+                        new SettingsPage()
+                    }
+                };
+
+                settingsIcon.FontSize = 12;
+                settingsIcon.Glyph = "\uE72B";
+                ToolTipService.SetToolTip(settingsButton, "Back");
+            }
+            else if (mainContent is null) //Set content to settings page
+            {
+                mainContent = Content as StackPanel;
+                mainContent.Children.Remove(titleBarGrid);
+
+                settingsPage.Children.Insert(0, titleBarGrid);
+                Content = settingsPage;
+
+                settingsIcon.FontSize = 12;
+                settingsIcon.Glyph = "\uE72B";
+                ToolTipService.SetToolTip(settingsButton, "Back");
+            }
+            else //Reset content to main content
+            {
+                settingsPage.Children.Remove(titleBarGrid);
+
+                mainContent.Children.Insert(0, titleBarGrid);
+                Content = mainContent;
+                mainContent = null;
+
+                settingsIcon.FontSize = 14;
+                settingsIcon.Glyph = "\uE713";
+                ToolTipService.SetToolTip(settingsButton, "Settings");
+            }
+        }
     }
 }
