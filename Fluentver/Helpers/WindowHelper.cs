5e843e1af350a365c1dc8867af87ef1d629a5b0d
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-26 19:07:17 -0700

    Add ActivateWindow method

--- Fluentver/Helpers/WindowHelper.cs
+++ Fluentver/Helpers/WindowHelper.cs
@@ -9,8 +9,13 @@ namespace Fluentver.Helpers
         [DllImport("uxtheme.dll", EntryPoint = "#135", SetLastError = true, CharSet = CharSet.Unicode)]
         private static extern int SetPreferredAppMode(ElementTheme preferredAppMode);
 
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
         #endregion
 
         public static void SetAppTheme(ElementTheme theme) => _ = SetPreferredAppMode(theme);
+
+        public static void ActivateWindow(IntPtr hWnd) => SetForegroundWindow(hWnd);
     }
 }
