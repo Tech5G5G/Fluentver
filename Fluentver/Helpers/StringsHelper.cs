3b9c0048e59959b275b6ad1db7c1ab8691031a41
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-04-07 12:03:34 -0700

    Add overload to GetString

--- Fluentver/Helpers/StringsHelper.cs
+++ Fluentver/Helpers/StringsHelper.cs
@@ -6,8 +6,18 @@ namespace Fluentver.Helpers
     public static class StringsHelper
     {
         readonly static ResourceLoader loader = new();
+        readonly static ResourceManager manager = new();
 
         public static string GetString(string id) => loader.GetString(id.Replace('.', '/'));
+
+        public static string GetString(string id, string language)
+        {
+            var context = manager.CreateResourceContext();
+            context.QualifierValues["Language"] = language;
+
+            var map = manager.MainResourceMap.GetSubtree("Resources");
+            return map.GetValue(id.Replace('.', '/'), context).ValueAsString;
+        }
     }
 
     public partial class StringResource : MarkupExtension
