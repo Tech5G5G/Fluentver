15d3e938067ade3f29ef5a83d9950dc1976e96a5
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-26 10:56:03 -0700

    Add MainPage

---
+++ Fluentver/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Fluver.Pages
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
