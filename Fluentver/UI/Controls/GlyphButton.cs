1ae7d0c71ce5d5e4ee07818f44cfb400cdd5a2c0
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-26 10:55:49 -0700

    Rewrite GlyphButton to be templated and inherit from ButtonBase

---
+++ Fluentver/UI/Controls/GlyphButton.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace Fluver.UI.Controls
+{
+    public sealed partial class GlyphButton : ButtonBase
+    {
+        public GlyphButton()
+        {
+            DefaultStyleKey = typeof(GlyphButton);
+        }
+
+        public static DependencyProperty GlyphSourceProperty { get; } =
+            DependencyProperty.Register(nameof(GlyphSource), typeof(IconSource), typeof(GlyphButton), new PropertyMetadata(defaultValue: null));
+
+        public IconSource GlyphSource
+        {
+            get => (IconSource)GetValue(GlyphSourceProperty);
+            set => SetValue(GlyphSourceProperty, value);
+        }
+    }
+}
