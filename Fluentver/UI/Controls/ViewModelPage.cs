a8a12f5b606ca6cb859deb37bc25f376ea3d34fd
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-26 22:21:15 -0700

    Add ViewModelPage + PageViewModel

---
+++ Fluentver/UI/Controls/ViewModelPage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Fluver.ViewModels;
+
+namespace Fluver.UI.Controls;
+
+public abstract partial class ViewModelPage : Page
+{
+    protected abstract PageViewModel PageViewModel { get; }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+        PageViewModel.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+        base.OnNavigatedFrom(e);
+        PageViewModel.OnNavigatedFrom(e);
+    }
+}
---
+++ Fluentver/ViewModels/PageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Navigation;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Fluver.ViewModels;
+
+public partial class PageViewModel : ObservableObject
+{
+    public virtual void OnNavigatedTo(NavigationEventArgs e) { }
+
+    public virtual void OnNavigatedFrom(NavigationEventArgs e) { }
+}
