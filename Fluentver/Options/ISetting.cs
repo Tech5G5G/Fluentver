04644c1692527a6c2e1491b013b6da7ecbe50a3c
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-29 11:17:42 -0700

    Add ISetting

---
+++ Fluentver/Options/ISetting.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Fluver.Options;
+
+public interface ISetting<T> : INotifyPropertyChanged
+{
+    event EventHandler<T> ValueChanged;
+
+    T Value { get; set; }
+}
