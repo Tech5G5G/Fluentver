d737c02aff6759d4dd204ed8c3a69fa09c8299ef
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-29 11:11:43 -0700

    Move WindowHelper into PInvoke static class

---
+++ Fluentver/System/Interop/PInvoke.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Fluver.System.Interop;
+
+internal static partial class PInvoke
+{
+    [LibraryImport("uxtheme.dll", EntryPoint = "#135")]
+    public static partial int SetPreferredAppMode(PreferredAppMode preferredAppMode);
+
+    [LibraryImport("uxtheme.dll", EntryPoint = "#136")]
+    public static partial void FlushMenuThemes();
+
+    [LibraryImport("user32.dll")]
+    private static partial nint GetWindowLongW(nint hWnd, int nIndex);
+
+    [LibraryImport("user32.dll")]
+    private static partial nint GetWindowLongPtrW(nint hWnd, int nIndex);
+
+    public static nint GetWindowLong(nint hWnd, int nIndex)
+    {
+        return Environment.Is64BitProcess ? GetWindowLongPtrW(hWnd, nIndex) : GetWindowLongW(hWnd, nIndex);
+    }
+
+    [LibraryImport("user32.dll")]
+    private static partial nint SetWindowLongW(nint hWnd, int nIndex, nint dwNewLong);
+
+    [LibraryImport("user32.dll")]
+    private static partial nint SetWindowLongPtrW(nint hWnd, int nIndex, nint dwNewLong);
+
+    public static nint SetWindowLong(nint hWnd, int nIndex, nint dwNewLong)
+    {
+        return Environment.Is64BitProcess ? SetWindowLongPtrW(hWnd, nIndex, dwNewLong) : SetWindowLongW(hWnd, nIndex, dwNewLong);
+    }
+
+    public const int GWL_HWNDPARENT = -8;
+}
---
+++ Fluentver/System/Interop/PreferredAppMode.cs
@@ -0,0 +1,7 @@
+﻿namespace Fluver.System.Interop;
+
+internal enum PreferredAppMode
+{
+    ForceDark = 2,
+    ForceLight = 3
+}
