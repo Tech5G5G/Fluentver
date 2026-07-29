fa0d5bc8819db6ded6943065539e634f26274214
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-29 11:16:24 -0700

    Switch to FluverPaghe

--- Fluentver/Navigation/MainPageNavigationService.cs
+++ Fluentver/Navigation/MainPageNavigationService.cs
@@ -1,20 +1,29 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Fluver.Views;
+using Fluver.Options;
 
 namespace Fluver.Navigation;
 
-public sealed class MainPageNavigationService : NavigationService<MainPagePage>, IMainPageNavigationService
+public sealed class MainPageNavigationService : NavigationService<FluverPage>, IMainPageNavigationService
 {
-    public override MainPagePage Page => Frame.Content switch
+    public override FluverPage CurrentPage => Frame.Content switch
     {
-        PCPage => MainPagePage.PCPage,
-        UsersPage => MainPagePage.UsersPage,
-        StoragePage => MainPagePage.StoragePage,
-        InsiderPage => MainPagePage.InsiderPage,
-        _ => MainPagePage.AboutPage
+        AboutPage => FluverPage.AboutPage,
+        PCPage => FluverPage.PCPage,
+        UsersPage => FluverPage.UsersPage,
+        StoragePage => FluverPage.StoragePage,
+        InsiderPage => FluverPage.InsiderPage,
+        _ => FluverPage.None
     };
 
-    protected override IReadOnlyDictionary<MainPagePage, Type> PageTypes { get; } = new Dictionary<MainPagePage, Type>
+    protected override IReadOnlyDictionary<FluverPage, Type> PageTypes { get; } = new Dictionary<FluverPage, Type>
+    {
+        { FluverPage.AboutPage, typeof(AboutPage) },
+        { FluverPage.PCPage, typeof(PCPage) },
+        { FluverPage.UsersPage, typeof(UsersPage) },
+        { FluverPage.StoragePage, typeof(StoragePage) },
+        { FluverPage.InsiderPage, typeof(InsiderPage) }
+    };
     {
         { MainPagePage.AboutPage, typeof(AboutPage) },
         { MainPagePage.PCPage, typeof(PCPage) },
@@ -33,4 +42,4 @@ public enum MainPagePage
     InsiderPage
 }
 
-public interface IMainPageNavigationService : INavigationService<MainPagePage>;
+public interface IMainPageNavigationService : INavigationService<FluverPage>;
