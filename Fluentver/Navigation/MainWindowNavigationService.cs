fc0f437cc1bc071a5e199f63b1f7f0dbc261af0d
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-07-29 11:17:21 -0700

    Implement transitions for MainWindow

--- Fluentver/Navigation/MainWindowNavigationService.cs
+++ Fluentver/Navigation/MainWindowNavigationService.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 using Fluver.Views;
 
 namespace Fluver.Navigation;
 
 public sealed class MainWindowNavigationService : NavigationService<MainWindowPage>, IMainWindowNavigationService
 {
-    public override MainWindowPage Page => Frame.Content switch
+    public override MainWindowPage CurrentPage => Frame.Content switch
     {
+        MainPage => MainWindowPage.MainPage,
         SettingsPage => MainWindowPage.SettingsPage,
-        _ => MainWindowPage.MainPage
+        _ => MainWindowPage.None
     };
 
     protected override IReadOnlyDictionary<MainWindowPage, Type> PageTypes { get; } = new Dictionary<MainWindowPage, Type>
@@ -16,10 +18,31 @@ public sealed class MainWindowNavigationService : NavigationService<MainWindowPa
         { MainWindowPage.MainPage, typeof(MainPage) },
         { MainWindowPage.SettingsPage, typeof(SettingsPage) }
     };
+
+    protected override NavigationTransitionInfo DetermineTransition(MainWindowPage oldPage, MainWindowPage newPage)
+    {
+        return oldPage switch
+        {
+            // First navigation - setting content of MainWindow
+            MainWindowPage.None => new SuppressNavigationTransitionInfo(),
+
+            // Navigating to settings page - slide from right (<-)
+            MainWindowPage.MainPage when newPage == MainWindowPage.SettingsPage =>
+                new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight },
+
+            // Navigating from settings page - slide from left (->)
+            MainWindowPage.SettingsPage when newPage == MainWindowPage.MainPage =>
+                new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromLeft },
+
+            // Unknown state - fall back to default (should never happen tho)
+            _ => base.DetermineTransition(oldPage, newPage)
+        };
+    }
 }
 
 public enum MainWindowPage
 {
+    None,
     MainPage,
     SettingsPage
 }
