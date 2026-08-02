cad99727ba06b5def26012336e8a55e45ffdcc49
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-08-02 14:08:28 -0700

    Remove unused methods

--- Fluentver/Extensions/ControlExtensions.cs
+++ Fluentver/Extensions/ControlExtensions.cs
@@ -1,27 +1,9 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
+﻿using Microsoft.UI.Xaml.Controls;
 
 namespace Fluver.Extensions;
 
 public static class ControlExtensions
 {
-    public static int GetSelectedIndex(this SelectorBar bar)
-    {
-        return bar.Items.IndexOf(bar.SelectedItem);
-    }
-
-    public static void SetSelectedIndex(this SelectorBar bar, int index)
-    {
-        bar.SelectedItem = index >= 0 && index < bar.Items.Count ? bar.Items[index] : null;
-    }
-
-    public static T AddClick<T>(this T button, RoutedEventHandler handler) where T : ButtonBase
-    {
-        button.Click += handler;
-        return button;
-    }
-
     /// <summary>
     /// Sets the text of a TextBlock if no text is currently selected.
     /// </summary>
