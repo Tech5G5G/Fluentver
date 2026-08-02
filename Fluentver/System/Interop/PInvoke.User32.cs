19c9f416443859a297604dca83c22cc436b90f67
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-08-02 14:14:28 -0700

    Add PInvoke.User32

---
+++ Fluentver/System/Interop/PInvoke.User32.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Fluver.System.Interop;
+
+internal static partial class PInvoke
+{
+    [LibraryImport("user32.dll")]
+    public static partial nint GetSystemMenu(nint hWnd, [MarshalAs(UnmanagedType.Bool)] bool bRevert);
+
+    [LibraryImport("user32.dll", EntryPoint = "InsertMenuW", StringMarshalling = StringMarshalling.Utf16)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool InsertMenu(
+        nint hMenu,
+        uint uPosition,
+        uint uFlags,
+        nuint uIDNewItem,
+        [MarshalAs(UnmanagedType.LPWStr), Optional] string lpNewItem);
+
+    public const uint SC_CLOSE = 0xF060;
+
+    public const uint MF_STRING = 0x00000000,
+                      MF_BYCOMMAND = 0x00000000,
+                      MF_SEPARATOR = 0x00000800;
+}
