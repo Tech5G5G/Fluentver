2cad6d36c0e185ee56bed01c704a8c1cf2b32a4b
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-08-04 17:44:42 -0700

    Modify startup page if not Insider build

--- Fluentver/Options/SettingsService.cs
+++ Fluentver/Options/SettingsService.cs
@@ -1,7 +1,20 @@
-﻿namespace Fluver.Options;
+﻿using Fluver.Helpers;
+
+namespace Fluver.Options;
 
 public sealed class SettingsService : ISettingsService
 {
     public ISetting<FluverPage> StartupPage { get; } = new EnumSetting<FluverPage>(nameof(StartupPage), FluverPage.AboutPage);
     public ISetting<BackdropType> Backdrop { get; } = new EnumSetting<BackdropType>(nameof(Backdrop), BackdropType.Mica);
+
+    public SettingsService()
+    {
+        if (!VersionHelper.IsWindowsInsider)
+        {
+            if (StartupPage.Value == FluverPage.InsiderPage)
+            {
+                StartupPage.Value = FluverPage.AboutPage;
+            }
+        }
+    }
 }
