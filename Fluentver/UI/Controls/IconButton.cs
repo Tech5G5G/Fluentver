f5d77ea4928c29bfdd8254573190258880f95518
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-08-04 19:35:58 -0700

    Update IconButton template and rebase to Button

---
+++ Fluentver/UI/Controls/IconButton.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Fluver.UI.Controls
+{
+    public sealed partial class IconButton : Button
+    {
+        public IconButton()
+        {
+            DefaultStyleKey = typeof(IconButton);
+        }
+
+        public IconElement Icon
+        {
+            get => (IconElement)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static DependencyProperty IconProperty { get; } =
+            DependencyProperty.Register(nameof(Icon), typeof(IconElement), typeof(IconButton), new PropertyMetadata(defaultValue: null));
+    }
+}
